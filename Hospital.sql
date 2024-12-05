597671b525a38ad3c01c9ca9e6ef6c6357897ab1
queolvi <korpachyovdmitriy@gmail.com>
2024-12-05 19:53:43 +0300

    finally version

--- Hospital.sql
+++ Hospital.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Patients (
+CREATE TABLE Patients(
 patient_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 patient_fio VARCHAR(255) NOT NULL,
 patient_birthday DATE NOT NULL,
@@ -6,13 +6,23 @@ patient_gender ENUM('M', 'F') NOT NULL,
 service_discount DECIMAL(5,2)
 );
 
-CREATE TABLE MedicalCards (
-med_card_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-patient_id INT UNSIGNED UNIQUE NOT NULL,
-FOREIGN KEY (patient_id) REFERENCES Patients(patient_id)
+CREATE TABLE Specialties(
+specialty_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+specialty VARCHAR(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE Doctors (
+CREATE TABLE Categories(
+category_number INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+category_name VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE Diagnoses(
+diagnosis_code INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+diagnosis_name VARCHAR(255) UNIQUE NOT NULL
+);
+
+
+CREATE TABLE Doctors(
 doctor_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 doctor_fio VARCHAR(255) NOT NULL,
 specialty_id INT UNSIGNED NOT NULL,
@@ -21,12 +31,13 @@ FOREIGN KEY (specialty_id) REFERENCES Specialties(specialty_id),
 FOREIGN KEY (category_number) REFERENCES Categories(category_number)
 );
 
-CREATE TABLE Specialties (
-specialty_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-specialty VARCHAR(255) UNIQUE NOT NULL
+CREATE TABLE MedicalCards(
+med_card_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+patient_id INT UNSIGNED UNIQUE NOT NULL,
+FOREIGN KEY (patient_id) REFERENCES Patients(patient_id)
 );
 
-CREATE TABLE Appointments (
+CREATE TABLE Appointments(
 appointment_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 doctor_id INT UNSIGNED NOT NULL,
 patient_id INT UNSIGNED NOT NULL,
@@ -37,17 +48,8 @@ FOREIGN KEY (doctor_id) REFERENCES Doctors(doctor_id),
 FOREIGN KEY (patient_id) REFERENCES Patients(patient_id)
 );
 
-CREATE TABLE Categories (
-category_number INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-category_name VARCHAR(255) UNIQUE NOT NULL
-);
 
-CREATE TABLE Diagnoses (
-diagnosis_code INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-diagnosis_name VARCHAR(255) UNIQUE NOT NULL
-);
-
-CREATE TABLE DoctorDiagnoses ( 
+CREATE TABLE DoctorDiagnoses( 
 doctor_id INT UNSIGNED NOT NULL,
 diagnosis_code INT UNSIGNED NOT NULL,
 PRIMARY KEY (doctor_id, diagnosis_code),
@@ -55,10 +57,11 @@ FOREIGN KEY (doctor_id) REFERENCES Doctors(doctor_id),
 FOREIGN KEY (diagnosis_code) REFERENCES Diagnoses(diagnosis_code)
 );
 
-CREATE TABLE MedicalCardDiagnoses ( 
+CREATE TABLE MedicalCardDiagnoses( 
 med_card_id INT UNSIGNED NOT NULL,
 diagnosis_code INT UNSIGNED NOT NULL,
 PRIMARY KEY (med_card_id, diagnosis_code),
 FOREIGN KEY (med_card_id) REFERENCES MedicalCards(med_card_id),
 FOREIGN KEY (diagnosis_code) REFERENCES Diagnoses(diagnosis_code)
 );
+
