03829fa18399388fe52622e10dae421ea58ca8d9
thekorp <146642604+queolvi@users.noreply.github.com>
2025-02-06 10:32:56 +0300

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,14 @@
+﻿USE Hospital
+
+UPDATE Appointments
+SET visit_cost = (visit_cost * 1.1) 
+WHERE doctor_id = 1;
+
+UPDATE Patients
+SET service_discount = service_discount - service_discount * 0.015
+WHERE patient_birthday < '2000-04-04';
+
+UPDATE Patients
+SET service_discount =service_discount - service_discount * 0.1
+WHERE patient_address NOT LIKE '%Ярославль%';
+
